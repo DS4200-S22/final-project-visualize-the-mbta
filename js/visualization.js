8de8d5c2778b7358d7d0c4937979451d1cb17b29
mikaelagordon <75488954+mikaelagordon@users.noreply.github.com>
2022-03-30 21:47:45 -0400

    map setup massachusetts

--- js/visualization.js
+++ js/visualization.js
@@ -1,22 +1,51 @@
-//This is filler -- delete it and start coding your visualization tool here
-d3.select("#vis-container")
-  .append("text")
-  .attr("x", 20)
-  .attr("y", 20)
-  .text("Hello World!");
 
-//d3.csv("/data/Line,_and_Stop.csv", function (data) {
-//console.log(data);
-//});
+const width = 960,
+    height = 500;
 
-d3.csv("/data/Line,_and_Stop.csv").then((data) => {
-  // d3.csv parses a csv file and passes the data
-  // to an anonymous function. Note how we build
-  // our visual inside of this anonymous function
+var projection = d3.geoMercator()
+    .center([109.25, 42.3])
+    .scale(50000)
+    .rotate([-180,0]);
 
-  // let's check our data
+var svg = d3.select("#vis-container").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-  for (let i = 0; i < 10; i++) {
-    console.log(data[i]);
-  }
+var path = d3.geoPath()
+    .projection(projection);
+
+var g = svg.append("g");
+
+// load and display massachusetts
+
+d3.json("data/mass_counties.json").then(function(topology) {
+
+    g.selectAll("path")
+       .data(topojson.feature(topology, topology.objects.cb_2015_massachusetts_county_20m).features)
+       .enter().append("path")
+       .attr("d", path);
+
 });
+
+
+/*
+
+d3.json("data/world-110m2.json").then(function(topology) {
+
+    g.selectAll("path")
+       .data(topojson.feature(topology, topology.objects.countries).features)
+       .enter().append("path")
+       .attr("d", path);
+
+});
+*/
+
+var zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on('zoom', function(event) {
+      g.selectAll('path')
+       .attr('transform', event.transform);
+});
+
+svg.call(zoom);
+
