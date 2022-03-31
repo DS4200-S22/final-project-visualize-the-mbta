83f9d5297d2a00a89dd07ba597db4a345ba6fead
mikaelagordon <75488954+mikaelagordon@users.noreply.github.com>
2022-03-30 22:01:33 -0400

    adding in map setup

--- js/visualization.js
+++ js/visualization.js
@@ -11,6 +11,62 @@ d3.csv("/data/Line,_and_Stop.csv").then((data) => {
   }
 });
 
+//MAP
+const width = 960,
+    height = 500;
+
+var projection = d3.geoMercator()
+    .center([109.25, 42.3])
+    .scale(50000)
+    .rotate([-180,0]);
+
+var mapsvg = d3.select("#vis-container").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var path = d3.geoPath()
+    .projection(projection);
+
+var g = mapsvg.append("g");
+
+// load and display massachusetts
+
+d3.json("data/mass_counties.json").then(function(topology) {
+
+    g.selectAll("path")
+       .data(topojson.feature(topology, topology.objects.cb_2015_massachusetts_county_20m).features)
+       .enter().append("path")
+       .attr("d", path);
+
+});
+
+
+/*
+
+d3.json("data/world-110m2.json").then(function(topology) {
+
+    g.selectAll("path")
+       .data(topojson.feature(topology, topology.objects.countries).features)
+       .enter().append("path")
+       .attr("d", path);
+
+});
+*/
+
+var zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on('zoom', function(event) {
+      g.selectAll('path')
+       .attr('transform', event.transform);
+});
+
+svg.call(zoom);
+
+
+
+
+
+
 // piechart code used from https://d3-graph-gallery.com/graph/pie_annotation.html
 
 // set the dimensions and margins of the graph
@@ -129,3 +185,5 @@ svg
   
       
   });
+
+
