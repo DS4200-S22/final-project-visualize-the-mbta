ce6e9c5e16125563b417a6470f9f579505aa1903
nole2701 <nole2701@gmail.com>
2022-03-30 20:15:37 -0400

    added table v1

--- js/visualization.js
+++ js/visualization.js
@@ -67,3 +67,65 @@ svg
   .attr("transform", function(d) { return `translate(${arcGenerator.centroid(d)})`})
   .style("text-anchor", "middle")
   .style("font-size", 17)
+
+
+
+  d3.csv("/data/Line,_and_Stop.csv").then((data) => {
+    // d3.csv parses a csv file and passes the data
+    // to an anonymous function. Note how we build
+    // our visual inside of this anonymous function
+  
+    // let's check our data
+  
+    var tableData = [];
+  
+    for (let i = 0; i < 10; i++) {
+      let row = [];
+      row["Station"] = data[i]["stop_name"];
+      row["Line(s)"] = data[i]["route_name"];
+      row["Ons"] = data[i]["average_ons"];
+      row["Offs"] = data[i]["average_offs"];
+      row["Average Flow"] = data[i]["average_flow"];
+  
+      tableData.push(row);
+    }
+    console.log(tableData);
+  
+    // Create table
+    var table = d3.select("#table-holder").append("table");
+    var thead = table.append("thead");
+    var tbody = table.append("tbody");
+  
+    var columns = ["Station", "Line(s)", "Ons", "Offs", "Average Flow"];
+  
+    thead.append("tr")
+      .selectAll("th")
+      .data(columns)
+      .enter()
+      .append("th")
+      .text(function(d,i){
+        return d;
+      })
+  
+    var rows = tbody.selectAll("tr")
+      .data(tableData)
+      .enter()
+      .append("tr");
+  
+    var cells = rows.selectAll("td")
+      .data(function(row) {
+        return columns.map(function(column) {
+          return {
+            column:column,
+            value:row[column]
+          }
+        })
+      })
+      .enter()
+      .append("td")
+      .text(function(d,i) {
+        return d.value;
+      })
+  
+      
+  });
